0bc462e0a94ad7c6bd71d38c25344ceb7f89d474
CodeSent <info@codesent.io>
2024-12-02 18:04:05 +0100

    [IMP] few tweaks

---
+++ apiproxy/resources/jsc/ParseContent.js
@@ -0,0 +1,24 @@
+var requestBody = context.getVariable("request.content");
+
+if (requestBody) {
+    try {
+        var parsedBody = JSON.parse(requestBody);
+        var name = parsedBody.name || "";
+        var dob = parsedBody.dob || "";
+        var securityQuestion = parsedBody.securityQuestion || "";
+
+        var xmlContent = 
+            '<User>' +
+                '<Name>' + name + '</Name>' +
+                '<DOB>' + dob + '</DOB>' +
+                '<SecurityQuestion>' + securityQuestion + '</SecurityQuestion>' +
+            '</User>';
+
+        context.setVariable("request.content", xmlContent);
+        context.setVariable("request.headers.content-type", "application/xml");
+    } catch (e) {
+        context.setVariable("error", "Invalid JSON");
+    }
+} else {
+    context.setVariable("error", "Empty Body");
+}
