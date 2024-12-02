61bf2bde0e7283e915118d0762b4136fadbd1f0c
CodeSent <info@codesent.io>
2024-12-02 17:43:21 +0100

    [IMP] bunch of new policies

---
+++ apiproxy/resources/jsc/ExtractJWT.js
@@ -0,0 +1,13 @@
+var authorizationHeader = context.getVariable("request.header.Authorization");
+
+if (authorizationHeader) {
+    var parts = authorizationHeader.split(' ');
+    if (parts.length === 2 && parts[0] === 'Bearer') {
+        var token = parts[1];
+        context.setVariable("flow.jwt", token);
+    } else {
+        throw new Error("Invalid token");
+    }
+} else {
+    throw new Error("Missed token");
+}
